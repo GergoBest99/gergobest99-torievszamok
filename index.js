f5d09eba9d58764caed753dfecf4f9e88c3ead17
GergoBest99 <120212701+GergoBest99@users.noreply.github.com>
2023-05-25 15:14:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem1 = document.getElementById("navbar-item1");
+  
+    navbarItem1.addEventListener("click", function() {
+      window.location.href = "index.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem2 = document.getElementById("navbar-item2");
+  
+    navbarItem2.addEventListener("click", function() {
+      window.location.href = "roma/roma.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem3 = document.getElementById("navbar-item3");
+  
+    navbarItem3.addEventListener("click", function() {
+      window.location.href = "kozepkor/kozepkor.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem4 = document.getElementById("navbar-item4");
+  
+    navbarItem4.addEventListener("click", function() {
+      window.location.href = "honfoglalas/honfoglalas.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem5 = document.getElementById("navbar-item5");
+  
+    navbarItem5.addEventListener("click", function() {
+      window.location.href = "anjou-haz/anjou-haz.html";
+    });
+});
