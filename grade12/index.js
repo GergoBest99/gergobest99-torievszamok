b109207772de6a2283795b84bb2abf479d73e263
GergoBest99 <120212701+GergoBest99@users.noreply.github.com>
2023-05-25 19:41:28 +0200

    Add files via upload

---
+++ grade12/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem1 = document.getElementById("navbar-item1");
+  
+    navbarItem1.addEventListener("click", function() {
+      window.location.href = "index.html";
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade9 = document.getElementById("grade-9");
+
+  grade9.addEventListener("click", function() {
+    window.location.href = "../index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade10 = document.getElementById("grade-10");
+
+  grade10.addEventListener("click", function() {
+    window.location.href = "../grade10/index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade11 = document.getElementById("grade-11");
+
+  grade11.addEventListener("click", function() {
+    window.location.href = "../grade11/index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade12 = document.getElementById("grade-12");
+
+  grade12.addEventListener("click", function() {
+    window.location.href = "index.html";
+  });
+});
