57b8100964f85d00c2a8f1d0a4b068572314ed96
GergoBest99 <120212701+GergoBest99@users.noreply.github.com>
2023-05-26 21:52:27 +0200

    Add files via upload

---
+++ honfoglalas/honfoglalas.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem1 = document.getElementById("navbar-item1");
+  
+    navbarItem1.addEventListener("click", function() {
+      window.location.href = "../index.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem2 = document.getElementById("navbar-item2");
+  
+    navbarItem2.addEventListener("click", function() {
+      window.location.href = "../roma/roma.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem3 = document.getElementById("navbar-item3");
+  
+    navbarItem3.addEventListener("click", function() {
+      window.location.href = "../kozepkor/kozepkor.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem4 = document.getElementById("navbar-item4");
+  
+    navbarItem4.addEventListener("click", function() {
+      window.location.href = "honfoglalas.html";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navbarItem5 = document.getElementById("navbar-item5");
+  
+    navbarItem5.addEventListener("click", function() {
+      window.location.href = "../anjou-haz/anjou-haz.html";
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade9 = document.getElementById("grade-9");
+
+  grade9.addEventListener("click", function() {
+    window.location.href = "../index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade10 = document.getElementById("grade-10");
+
+  grade10.addEventListener("click", function() {
+    window.location.href = "../grade10/index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade11 = document.getElementById("grade-11");
+
+  grade11.addEventListener("click", function() {
+    window.location.href = "../grade11/index.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var grade12 = document.getElementById("grade-12");
+
+  grade12.addEventListener("click", function() {
+    window.location.href = "../grade12/index.html";
+  });
+});
